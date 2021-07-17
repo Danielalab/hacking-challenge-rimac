f2c3c0a69653f02985654b2a2bddf053baf91946
Danielalab <olabolacrayola@hotmail.com>
2021-07-17 02:51:46 -0500

    feat(pages): add LoginView component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import logo from './logo.svg';
-import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import Header from './components/common/Header';
+import LoginView from './pages/LoginView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/">
+            <LoginView />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/pages/LoginView.jsx
@@ -0,0 +1,11 @@
+import LoginForm from '../components/Login/LoginForm';
+import WelcomeBanner from '../components/Login/WelcomeBanner';
+
+const LoginView = () => (
+  <div>
+    <WelcomeBanner />
+    <LoginForm />
+  </div>
+);
+
+export default LoginView;
