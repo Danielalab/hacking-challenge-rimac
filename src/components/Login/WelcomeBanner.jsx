c8b1492dc61e33103279992075c05b14c78b362f
Danielalab <olabolacrayola@hotmail.com>
2021-07-17 18:55:05 -0500

    style(WelcomeBanner): add desktop styles

--- src/components/Login/WelcomeBanner.jsx
+++ src/components/Login/WelcomeBanner.jsx
@@ -1,13 +1,21 @@
 import './scss/WelcomeBanner.scss';
 import imageBannerMobile from '../../images/woman-with-phone.png';
+import imageBannerDesktop from '../../images/illustracion-banner-desktop.png';
 
 const WelcomeBanner = () => (
-  <section className="d-flex position-relative container container--padding banner">
-    <div>
+  <section className="d-flex flex-lg-column justify-content-center
+    position-relative container-fluid container--padding banner"
+  >
+    <figure className="banner__content--padding banner__image-dektop d-none d-lg-block">
+      <img src={imageBannerDesktop} alt="mujer sonriente con un celular y auto" />
+    </figure>
+    <div className="banner__content--padding">
       <p className="mb-2 title--font title--color">¡Nuevo!</p>
       <h1 className="mb-3 title--font title--color">
-        Seguro Vehicular
-        <span> Tracking</span>
+        Seguro
+        {' '}
+        <span className="banner__title-desktop--color">Vehicular</span>
+        <span className="banner__title--color"> Tracking</span>
       </h1>
       <p className="m-0 paragraph--color">
         Cuentanos donde le haras
@@ -16,7 +24,7 @@ const WelcomeBanner = () => (
         seguimiento a tu seguro
       </p>
     </div>
-    <figure className="banner__image-mobile">
+    <figure className="banner__image-mobile d-lg-none">
       <img src={imageBannerMobile} alt="mujer sonriente con un celular y auto" />
     </figure>
   </section>
