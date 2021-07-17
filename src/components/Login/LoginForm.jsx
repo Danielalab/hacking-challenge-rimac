a1f2774e326a740adc643cf10e0747fa38aa5bc4
Danielalab <olabolacrayola@hotmail.com>
2021-07-17 18:57:05 -0500

    style(LoginView): add desktop styles

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -42,92 +42,94 @@ const LoginForm = () => {
   };
 
   return (
-    <form
-      className="form container container--padding needs-validation"
-      onSubmit={handleSubmit}
-    >
-      <h2 className="form__title--color mb-4">Déjanos tus datos</h2>
-      <div className="input-group mb-3">
-        <Select
-          id="document-type"
-          value={documentType}
-          onChange={(event) => eventHandler(setDocumentType)(event.target.value)}
-          required
-        >
-          <option value="DNI">DNI</option>
-          <option value="CE">CE</option>
-        </Select>
-        <InputField
-          label="Nro. de doc"
-          type="text"
-          className="form-control"
-          id="document-number"
-          placeholder="Nro. de doc"
-          onChange={(event) => eventHandler(setDocumentNumber)(event.target.value)}
-          value={documentNumber}
-          maxLength={8}
-          pattern="[0-9]{8}"
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <InputField
-          label="Celular"
-          type="text"
-          className="form-control"
-          id="mobile-phone"
-          placeholder="Celular"
-          value={mobilePhone}
-          onChange={(event) => eventHandler(setMobilePhone)(event.target.value)}
-          maxLength={9}
-          pattern="[0-9]{9}"
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <InputField
-          label="Placa"
-          type="text"
-          className="form-control"
-          id="car-plate"
-          placeholder="Placa: ABC-123"
-          value={carPlate.toUpperCase()}
-          onChange={(event) => eventHandler(setCarPlate)(event.target.value)}
-          maxLength={7}
-          pattern="[a-zA-Z0-9]{3}-[0-9]{3}"
-          required
-        />
-      </div>
-      <div className="form-check mb-4">
-        <input
-          className="form-check-input"
-          type="checkbox"
-          id="checkbox-conditions"
-          onChange={(event) => eventHandler(setAgreedToConditions)(event.target.checked)}
-          checked={agreedToConditions}
-          required
-        />
-        <label className="form-check-label form-check-label--color" htmlFor="checkbox-conditions">
-          Acepto la
-          {' '}
-          <a href="/">Política de Protección de Datos Personales</a>
-          {' '}
-          y los
-          {' '}
-          <a href="/">Términos y Condiciones</a>
-          .
-        </label>
-      </div>
-      <div>
-        <Button
-          type="submit"
-          color="primary"
-          classes="w-100"
-        >
-          COTÍZALO
-        </Button>
-      </div>
-    </form>
+    <div className="container--full-height d-flex align-items-center">
+      <form
+        className="form container-fluid needs-validation"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="form__title--color mb-4">Déjanos tus datos</h2>
+        <div className="input-group mb-3">
+          <Select
+            id="document-type"
+            value={documentType}
+            onChange={(event) => eventHandler(setDocumentType)(event.target.value)}
+            required
+          >
+            <option value="DNI">DNI</option>
+            <option value="CE">CE</option>
+          </Select>
+          <InputField
+            label="Nro. de doc"
+            type="text"
+            className="form-control"
+            id="document-number"
+            placeholder="Nro. de doc"
+            onChange={(event) => eventHandler(setDocumentNumber)(event.target.value)}
+            value={documentNumber}
+            maxLength={8}
+            pattern="[0-9]{8}"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <InputField
+            label="Celular"
+            type="text"
+            className="form-control"
+            id="mobile-phone"
+            placeholder="Celular"
+            value={mobilePhone}
+            onChange={(event) => eventHandler(setMobilePhone)(event.target.value)}
+            maxLength={9}
+            pattern="[0-9]{9}"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <InputField
+            label="Placa"
+            type="text"
+            className="form-control"
+            id="car-plate"
+            placeholder="Placa: ABC-123"
+            value={carPlate.toUpperCase()}
+            onChange={(event) => eventHandler(setCarPlate)(event.target.value)}
+            maxLength={7}
+            pattern="[a-zA-Z0-9]{3}-[0-9]{3}"
+            required
+          />
+        </div>
+        <div className="form-check mb-4">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id="checkbox-conditions"
+            onChange={(event) => eventHandler(setAgreedToConditions)(event.target.checked)}
+            checked={agreedToConditions}
+            required
+          />
+          <label className="form-check-label form-check-label--color" htmlFor="checkbox-conditions">
+            Acepto la
+            {' '}
+            <a href="/">Política de Protección de Datos Personales</a>
+            {' '}
+            y los
+            {' '}
+            <a href="/">Términos y Condiciones</a>
+            .
+          </label>
+        </div>
+        <div>
+          <Button
+            type="submit"
+            color="primary"
+            classes="w-100"
+          >
+            COTÍZALO
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- src/pages/LoginView.jsx
+++ src/pages/LoginView.jsx
@@ -2,9 +2,15 @@ import LoginForm from '../components/Login/LoginForm';
 import WelcomeBanner from '../components/Login/WelcomeBanner';
 
 const LoginView = () => (
-  <div>
-    <WelcomeBanner />
-    <LoginForm />
+  <div className="container-fluid">
+    <div className="row container--height">
+      <div className="col-lg-5 p-0">
+        <WelcomeBanner />
+      </div>
+      <div className="col-lg-7 p-0">
+        <LoginForm />
+      </div>
+    </div>
   </div>
 );
 
