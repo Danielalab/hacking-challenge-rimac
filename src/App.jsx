a856268c0a35e63462b8ac95148429744062345f
Danielalab <olabolacrayola@hotmail.com>
2021-07-17 16:34:03 -0500

    feat(context): add UserContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
+import { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
 
+/* Components */
 import Header from './components/common/Header';
 import LoginView from './pages/LoginView';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/">
-            <LoginView />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+/* Context */
+import { UserContext } from './context/UserContext';
 
+function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/">
+              <LoginView />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
