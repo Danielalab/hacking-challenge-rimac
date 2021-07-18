fcf7dd13061216776fcfcfe0ed6b16a5dbb1791a
Danielalab <olabolacrayola@hotmail.com>
2021-07-18 16:47:54 -0500

    feat(pages): add QuoteYourInsurenceView component

--- src/pages/QuoteYourInsurenceView.jsx
+++ src/pages/QuoteYourInsurenceView.jsx
@@ -2,23 +2,35 @@ import {
   Switch,
   Route,
   useRouteMatch,
+  useLocation,
 } from 'react-router-dom';
-import CarDataView from '../components/QuoteYourInsurence/CarDataView';
+import CarDataForm from '../components/QuoteYourInsurence/CarDataForm';
 import StepsProgress from '../components/QuoteYourInsurence/StepsProgress';
 
 const QuoteYourInsurenceView = () => {
   const { path } = useRouteMatch();
+  const { pathname } = useLocation();
+  const steps = [
+    { name: 'Datos del auto', pathname: `${path}/datos-del-auto` },
+    { name: 'Arma tu plan', pathname: `${path}/arma-tu-plan` },
+  ];
+  const currentStep = steps.findIndex((step) => step.pathname === pathname);
+
   return (
     <div className="container-fluid">
-      <StepsProgress />
-      <Switch>
-        <Route path={`${path}/datos-del-auto`}>
-          <CarDataView />
-        </Route>
-        <Route path={`${path}/arma-tu-plan`}>
-          arma tu plan component
-        </Route>
-      </Switch>
+      <div className="row">
+        <StepsProgress steps={steps.length} currentStep={currentStep + 1} />
+        <div className="col-lg-7 p-0">
+          <Switch>
+            <Route path={`${path}/datos-del-auto`}>
+              <CarDataForm />
+            </Route>
+            <Route path={`${path}/arma-tu-plan`}>
+              arma tu plan component
+            </Route>
+          </Switch>
+        </div>
+      </div>
     </div>
   );
 };
