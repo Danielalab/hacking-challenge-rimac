2aa8993461533646d610457fb8998be6596dcf0d
Danielalab <olabolacrayola@hotmail.com>
2021-07-18 17:52:36 -0500

    feat(utils): add fn to get current year

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,5 +1,8 @@
 export const eventHandler = (setStateCallback) => (value) => { setStateCallback(value); };
 
+export const getCurrentYear = () => new Date().getFullYear();
+
 export default {
   eventHandler,
+  getCurrentYear,
 };
