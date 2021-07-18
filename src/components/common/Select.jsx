fb38613e5bf8322e76040e6ece8f89d153b7417f
Danielalab <olabolacrayola@hotmail.com>
2021-07-18 14:50:09 -0500

    feat(Input): add label & floating properties

--- src/components/common/Select.jsx
+++ src/components/common/Select.jsx
@@ -3,22 +3,47 @@ import './scss/Select.scss';
 
 const Select = ({
   id,
+  label,
   children,
   required,
-}) => (
-  <select className="form-select select--color" id={id} required={required}>
-    {children}
-  </select>
-);
+  floating,
+  ...rest
+}) => {
+  const SelectElement = () => (
+    <select
+      {...rest}
+      className="form-select select--color"
+      id={id}
+      required={required}
+      aria-label={label}
+    >
+      {children}
+    </select>
+  );
+
+  return (
+    floating
+      ? (
+        <div className="form-floating">
+          <SelectElement />
+          {label && <label htmlFor={id}>{label}</label>}
+        </div>
+      )
+      : <SelectElement />
+  );
+};
 
 Select.defaultProps = {
   required: false,
+  floating: true,
 };
 
 Select.propTypes = {
   id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
   children: PropTypes.element.isRequired,
   required: PropTypes.bool,
+  floating: PropTypes.bool,
 };
 
 export default Select;
