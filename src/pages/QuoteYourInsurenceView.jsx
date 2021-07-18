1835bb2483c1c9b3121be170ed8fdc9b2e3db302
Danielalab <olabolacrayola@hotmail.com>
2021-07-18 17:54:49 -0500

    feat(QuoteYourInsurence): save year & model of car

--- src/pages/QuoteYourInsurenceView.jsx
+++ src/pages/QuoteYourInsurenceView.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Switch,
   Route,
@@ -6,10 +7,15 @@ import {
 } from 'react-router-dom';
 import CarDataForm from '../components/QuoteYourInsurence/CarDataForm';
 import StepsProgress from '../components/QuoteYourInsurence/StepsProgress';
+import { getCurrentYear } from '../components/utils';
 
 const QuoteYourInsurenceView = () => {
   const { path } = useRouteMatch();
   const { pathname } = useLocation();
+  const currentYear = getCurrentYear();
+  const [yearOfCar, setYearOfCar] = useState(currentYear);
+  const [modelOfCar, setmodelOfCar] = useState('Wolkswagen');
+
   const steps = [
     { name: 'Datos del auto', pathname: `${path}/datos-del-auto` },
     { name: 'Arma tu plan', pathname: `${path}/arma-tu-plan` },
@@ -23,7 +29,15 @@ const QuoteYourInsurenceView = () => {
         <div className="col-lg-7 p-0">
           <Switch>
             <Route path={`${path}/datos-del-auto`}>
-              <CarDataForm />
+              <CarDataForm {
+                ... {
+                  yearOfCar,
+                  setYearOfCar,
+                  modelOfCar,
+                  setmodelOfCar,
+                }
+              }
+              />
             </Route>
             <Route path={`${path}/arma-tu-plan`}>
               arma tu plan component
