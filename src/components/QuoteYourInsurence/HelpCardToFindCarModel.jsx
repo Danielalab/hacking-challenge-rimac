48c89a64b97be4ad9c37fa5a976b2d6d272c1a06
Danielalab <olabolacrayola@hotmail.com>
2021-07-19 13:40:06 -0500

    feat(QuoteInsurence): add HelpCardToFindCarModel component

--- src/components/QuoteYourInsurence/HelpCardToFindCarModel.jsx
+++ src/components/QuoteYourInsurence/HelpCardToFindCarModel.jsx
@@ -3,16 +3,19 @@ import './scss/HelpCardToFindCarModel.scss';
 import iconCar from '../../images/icon_car.svg';
 
 const HelpCardToFindCarModel = () => (
-  <div className="help-card--background d-flex align-items-center p-3 mb-3">
-    <figure className="mx-1 mb-0">
-      <img src={iconCar} alt="ícono de un carro" />
-    </figure>
-    <p className="mx-1 mb-0">
-      ¿No encuentras el modelo?
-      <br />
-      <span className="help-card__link title--font">CLIC AQUÍ</span>
-    </p>
-  </div>
+  <section className="help-card help-card--position help-card--background mb-3">
+    <h5 className="help-card__title title--font text-uppercase d-none d-lg-block border-bottom pb-3">ayuda</h5>
+    <div className="help-card__message d-flex flex-lg-row-reverse align-items-center">
+      <figure className="mx-1 mb-0">
+        <img src={iconCar} alt="ícono de un carro" />
+      </figure>
+      <p className="mx-1 mb-0">
+        ¿No encuentras el modelo?
+        <br />
+        <span className="help-card__link cursor--pointer title--font">CLIC AQUÍ</span>
+      </p>
+    </div>
+  </section>
 );
 
 export default HelpCardToFindCarModel;
