01750203e18a582237db33820c164b44d79c6f46
Danielalab <olabolacrayola@hotmail.com>
2021-07-20 15:55:54 -0500

    feat(App): show ThanksView from button "lo quiero"

--- src/components/QuoteYourInsurence/MakeYourPlan/PolicyPrice.jsx
+++ src/components/QuoteYourInsurence/MakeYourPlan/PolicyPrice.jsx
@@ -2,50 +2,63 @@ import PropTypes from 'prop-types';
 /* styles */
 import './scss/PolicyPrice.scss';
 /* components */
+import { useHistory } from 'react-router-dom';
 import Button from '../../common/Button';
 import badgeImage from '../../../images/badge-vehicular-flexible.png';
 
-const PolicyPrice = ({ total }) => (
-  <div className="policy-price container-fluid">
-    <div className="d-flex flex-lg-column justify-content-between">
-      <div className="align-self-end d-lg-flex full-width">
-        <div className="d-lg-flex flex-lg-column align-items-lg-center justify-content-lg-between">
-          <p className="h2 title-font title-color mb-1">
-            $
-            {total}
-            .00
-          </p>
-          <span className="policy-price__text--small text-uppercase d-lg-none">Mensual</span>
-          <span className="policy-price__text--small text-uppercase d-none d-lg-block">Mensuales</span>
+const PolicyPrice = ({ total }) => {
+  const history = useHistory();
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    history.push('/cotiza-tu-seguro/arma-tu-plan');
+  };
+
+  return (
+    <div className="policy-price container-fluid">
+      <div className="d-flex flex-lg-column justify-content-between">
+        <div className="align-self-end d-lg-flex full-width">
+          <div className="d-lg-flex flex-lg-column align-items-lg-center justify-content-lg-between">
+            <p className="h2 title-font title-color mb-1">
+              $
+              {total}
+              .00
+            </p>
+            <span className="policy-price__text--small text-uppercase d-lg-none">Mensual</span>
+            <span className="policy-price__text--small text-uppercase d-none d-lg-block">Mensuales</span>
+          </div>
+          <figure className="d-none d-lg-block mx-1 mb-0 ms-auto">
+            <img src={badgeImage} alt="Insignia vehicular flexible" />
+          </figure>
         </div>
-        <figure className="d-none d-lg-block mx-1 mb-0 ms-auto">
-          <img src={badgeImage} alt="Insignia vehicular flexible" />
-        </figure>
-      </div>
-      <hr className="d-none d-lg-block my-4" />
-      <div className="align-self-end full-width">
-        <p className="d-none d-lg-block">El precio incluye:</p>
-        <ul className="list-unstyled d-none d-lg-block mb-4">
-          <li className="mb-2">
-            <i className="bi bi-check2 me-3" />
-            <span>Llanta de respuesto</span>
-          </li>
-          <li className="mb-2">
-            <i className="bi bi-check2 me-3" />
-            <span>Analisis de motor</span>
-          </li>
-          <li className="mb-2">
-            <i className="bi bi-check2 me-3" />
-            <span>Aros gratis</span>
-          </li>
-        </ul>
-        <Button classes="policy-price__button">
-          <>LO QUIERO</>
-        </Button>
+        <hr className="d-none d-lg-block my-4" />
+        <div className="align-self-end full-width">
+          <p className="d-none d-lg-block">El precio incluye:</p>
+          <ul className="list-unstyled d-none d-lg-block mb-4">
+            <li className="mb-2">
+              <i className="bi bi-check2 me-3" />
+              <span>Llanta de respuesto</span>
+            </li>
+            <li className="mb-2">
+              <i className="bi bi-check2 me-3" />
+              <span>Analisis de motor</span>
+            </li>
+            <li className="mb-2">
+              <i className="bi bi-check2 me-3" />
+              <span>Aros gratis</span>
+            </li>
+          </ul>
+          <Button
+            classes="policy-price__button"
+            onClick={handleClick}
+          >
+            <>LO QUIERO</>
+          </Button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default PolicyPrice;
 
